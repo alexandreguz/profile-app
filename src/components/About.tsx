bbefcdf48d7447ea64f2d50e6c0ebd33e7343fb7
Alexandre Guz <guzalexandre@gmail.com>
2024-07-16 14:36:08 +0300

    beginning of about page

---
+++ src/components/About.tsx
@@ -0,0 +1,14 @@
+
+export default function About() {
+    return (
+        <>
+        <div className="flex content-normal items-center">
+            <div className="text-6xl font-bold w-1/3 ml-40 text-left text-gray-700 mt-32 border-r-4 border-red-600">Something About Law Firm</div>
+            <div className="text-2xl text-left w-2/5 text-gray-600 mt-32 ml-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilistyu quis odit, optio, dolorum est enim magni id voluptatum perspiciatis dicta voluptates quisquam, quam velit non consectetur vero obcaecati consequuntur repudiandae.</div>
+        </div>
+
+
+        </>
+        
+    )
+}
