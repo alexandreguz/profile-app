1aee83ba6ed5f81b08905b486285f692bef1f23f
Alexandre Guz <guzalexandre@gmail.com>
2024-07-26 06:38:51 +0300

    documents component

--- src/app/(application)/documents/page.tsx
+++ src/app/(application)/documents/page.tsx
@@ -1,8 +1,10 @@
+import Documents from "@/components/Documents";
+
 export default function documents () {
     return (
         <>
         <div className="p-4 sm:ml-64 bg-yellow-700 h-screen">
-          <div> documents</div>
+          <div> <Documents/> </div>
           </div>
           </>
     )
